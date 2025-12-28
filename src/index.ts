9655542455b7a86f30310fbb379c26dc89967d0d
opecgame <me@opecgame.in.th>
2025-12-29 04:58:21 +0700

    feat: v1 api

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import { BeamCheckout } from './lib/BeamCheckout'
+import { BeamCheckout } from "./lib/v0/BeamCheckout";
+import { BeamCheckoutV1 } from "./lib/v1/BeamCheckout";
 
-export default BeamCheckout
-export * from './lib/types'
+export default BeamCheckout;
+export { BeamCheckoutV1 };
+
+export * from "./lib/v0/types";
+export * as V1Types from "./lib/v1/types";
